a8e1252ce53d586befb47009e280607ce605a008
vvlee <vvlee@gmail.com>
2023-07-24 23:26:38 +0500

    change lesson2 homework

--- src/main/java/org/example/lesson2/homeWork200/Task203.java
+++ src/main/java/org/example/lesson2/homeWork200/Task203.java
@@ -10,15 +10,17 @@ public class Task203 {
             System.out.println(a / b);
             printSum(23, 234);
             Integer[] abc = {1, 2}; // установить ссылочный тип данных
-            System.out.println("abc = " + Arrays.toString(abc)); // добавить вывод
-            abc[1] = 9;
-            System.out.println("Новый abc = " + Arrays.toString(abc)); // добавить вывод
+            System.out.println("Начальный массив abc = " + Arrays.toString(abc)); // добавить вывод
+            int i = 3; // Добавить переменную для индексов массива
+            abc[i] = 9;
+            System.out.println("abc[" + i + "] = " + abc[i]); // Добавить вывод изменения элемента массива по индексу
+            System.out.println("Новый массив abc = " + Arrays.toString(abc)); // добавить вывод
 //        } catch (Throwable ex) { // Удалить класс высшего порядка для исключений
 //            System.out.println("Что-то пошло не так...");
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
+            System.out.println("Новый массив выходит за пределы начального размера!");
         }
     }
 
