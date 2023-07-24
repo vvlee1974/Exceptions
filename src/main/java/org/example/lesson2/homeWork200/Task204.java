e55bc2167b962d719ad14376758c8bf1549e07a9
vvlee <vvlee@gmail.com>
2023-07-24 22:30:26 +0500

    change lesson2 homework

---
+++ src/main/java/org/example/lesson2/homeWork200/Task204.java
@@ -0,0 +1,16 @@
+package org.example.lesson2.homeWork200;
+
+import java.util.Scanner;
+
+// Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+// Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+public class Task204 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String message = scanner.nextLine();
+        if (message == null || message.trim().isEmpty()){
+            System.out.println("Пустая строка недопустима. Повторите ввод текста.");
+        }else System.out.println("message = " + message);
+    }
+}
