d45f037729db9d6c6e14f46ef83fd07f93b8ee73
vvlee <vvlee@gmail.com>
2023-08-02 01:01:29 +0500

    change lesson3 homework

--- src/main/java/org/example/lesson3/homeWork300/Main.java
+++ src/main/java/org/example/lesson3/homeWork300/Main.java
@@ -1,5 +1,6 @@
 package org.example.lesson3.homeWork300;
 
+import org.example.lesson3.homeWork300.exceptions.MyStringFormatName;
 import org.example.lesson3.homeWork300.exceptions.MyStringFormatSex;
 
 import java.util.ArrayList;
@@ -9,22 +10,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите данные (ФИО ДатаРождения НомерТелефона Пол):");
         List<String> arrayInput = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             arrayInput.add(null);
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите данные (ФИО ДатаРождения НомерТелефона Пол):");
 
         System.out.println("Введите ФИО:");
         String input = scanner.nextLine();
+        arrayInput.set(0, getFullName(input));
+
+        System.out.println("Введите Пол (f/m):");
+        String input3 = scanner.nextLine();
+        arrayInput.set(3, getInfoSex(input3));
+
+
+        for (String strings : arrayInput) {
+            System.out.println(Arrays.toString(new String[]{strings}));
+        }
+    }
+
+    private static String getFullName(String string) {
+        String input = string.trim().replaceAll("\\s+", " ");
         String[] str = input.split(" ");
 
-        String sex ;
-        for (String s : str) {
-            if (str.length == 1 && (s.contains("f") || s.contains("m"))) {
+        if (str.length == 3) {
+            return input;
+        } else {
+            try {
+                throw new RuntimeException();
+            } catch (RuntimeException e) {
+                try {
+                    throw new MyStringFormatName();
+                } catch (MyStringFormatName ex) {
+                    System.out.println(ex.getMessage());
+                    System.exit(0);
+                }
+            }
+        }
+        return null;
+    }
+
+    private static String getInfoSex(String input3) {
+        String[] str3 = input3.split(" ");
+        String sex;
+        for (String s : str3) {
+            if (str3.length == 1 && (s.contains("f") || s.contains("m"))) {
                 sex = s;
-                arrayInput.set(3, sex);
+                return sex;
             } else {
                 try {
                     throw new RuntimeException();
@@ -33,13 +67,11 @@ public class Main {
                         throw new MyStringFormatSex();
                     } catch (MyStringFormatSex ex) {
                         System.out.println(ex.getMessage());
+                        System.exit(0);
                     }
                 }
             }
-
         }
-        for (String strings : arrayInput) {
-            System.out.println(Arrays.toString(new String[]{strings}));
-        }
+        return null;
     }
 }
--- src/main/java/org/example/lesson3/homeWork300/exceptions/MyStringFormatName.java
+++ src/main/java/org/example/lesson3/homeWork300/exceptions/MyStringFormatName.java
@@ -2,6 +2,6 @@ package org.example.lesson3.homeWork300.exceptions;
 
 public class MyStringFormatName extends RuntimeException{
     public MyStringFormatName() {
-        super("Введены не полные данные ФИО. Повторите ввод.");
+        super("Введены некорректные данные ФИО. Повторите ввод.");
     }
 }
